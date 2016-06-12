0a0f9812c08a3f2e87a1c57ff496c4d9b73d6dd4
aonisai <seiunryou0602@gmail.com>
2016-06-12 15:35:19 +0900

    Add ave.sh

---
+++ ave.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# time:数値の抽出結果を代入
+
+#数値と,以外を削除
+time=`cat $1 | sed -e 's/[^0-9.]//g'`
+
+sum=0
+
+#timeに格納された値の合計を計算
+for i in $time
+do
+    sum=`echo $sum + $i | bc`
+    #echo $sum
+done
+
+ave=`echo "scale=3; $sum / $i" | bc`
+echo $sum,$ave  #合計と平均

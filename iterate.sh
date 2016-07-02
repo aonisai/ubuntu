b480ad3155ae2785ab5aedefe5c57586b05d292a
aonisai <seiunryou0602@gmail.com>
2016-07-02 18:22:03 +0900

    change iterate.sh

--- iterate.sh
+++ iterate.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 2 ]; then #less than
 	echo "繰り返しの回数とコマンドを入力してください."
 	echo "e.g. 3 echo \"hello world\""
 	exit 1
 fi
 
 count=1
-for arg in "$@"
+echo $#,$@
+for arg in "$@" #引数をargに代入
 do
-	if [ $count -gt 2 ]; then
-		argstr=$argstr" "$arg
+	if [ $count -gt 2 ]; then #greater than
+		argstr=$argstr" "$arg #argstrに繰り返すコマンドの引数を代入
+		echo $count, $argstr
 	fi
 	count=`expr $count + 1`
 done
 
 for i in `seq 1 1 $1`
 do
+	echo $2
 	$2 $argstr
 done

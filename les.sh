c23db1bdd5f6290b8fdfb4f040884f72f52475fd
aonisai <seiunryou0602@gmail.com>
2016-07-04 01:36:03 +0900

    modified les.sh

--- les.sh
+++ les.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-file $1 | grep -o directory > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-	ls $1
+if [ $# -eq 0 ]; then
+	ls --color=auto
 else
-	less $1
-	#cat $1
+	file $1 | grep -o directory > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		ls --color=auto $1
+	else
+		less $1
+	fi
 fi

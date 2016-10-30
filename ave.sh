0a20ae118d8a7fd7cf119c1a2ea72abbc9a42262
aonisai <seiunryou0602@gmail.com>
2016-10-31 02:12:06 +0900

    ave.sh

--- ave.sh
+++ ave.sh
@@ -6,13 +6,15 @@
 time=`cat $1 | sed -e 's/[^0-9.]//g'`
 
 sum=0
-count=0
+#count=0
 #timeに格納された値の合計を計算
 for i in $time
 do
     sum=`echo $sum + $i | bc`
     #echo $sum
-	count=$(($count + 1))
+	count=$((count + 1))
+	#$[count++]
+	#echo $count
 done
 
 ave=`echo "scale=3; $sum / $count" | bc`

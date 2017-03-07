fc5fdf0de6cb4edab034359f0ee5848030a78c21
aonisai <seiunryou0602@gmail.com>
2017-03-07 15:56:03 +0900

    modify les.sh

--- les.sh
+++ les.sh
@@ -3,10 +3,10 @@
 if [ $# -eq 0 ]; then
 	ls --color=auto
 else
-	file $1 | grep -o directory > /dev/null 2>&1
+	file "$1" | grep -o directory > /dev/null 2>&1
 	if [ $? -eq 0 ]; then
-		ls --color=auto $1
+		ls --color=auto "$1"
 	else
-		less $1
+		less "$1"
 	fi
 fi
